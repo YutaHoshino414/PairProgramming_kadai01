8b5d8a68520f37e28ae796872f740e5d2e8f31ce
Yuta Hoshino <y.hoshino25@gmail.com>
2020-11-18 19:09:22 +0900

    JSファイル追加

---
+++ index.js
@@ -0,0 +1,14 @@
+let hoge = function(){
+let sample = document.getElementById('company_name');
+sample.innerHTML = "Hoshino";
+}
+
+$(function(){
+$('.contact-title').text('test');
+});
+
+// $(document).ready(function() {  
+//   $('#change_btn').on('click', function() {    
+//     $(".contact-title").css('color', 'blue');  
+// })
+// })
